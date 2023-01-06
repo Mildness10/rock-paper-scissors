60685ed3508eab21c2915d707af27a77701a4c9f
Mildness10 <m.akomoize@gmail.com>
2023-01-05 23:56:47 -0500

    initialized the template for the game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,9 +1,11 @@
-from tkinter import *
-# from countdown import countdown
+import random
 
-root = Tk()
+# WELCOME MESSAGE
+print('\nWELCOME TO ROCK, PAPER & SCISSORS 🎮')
 
-game_intro = Label(root, text="Welcome to the classic Rock, Paper & Scissors Game!")
-game_intro.pack()
+# ALLOW PLAYER TO PICK NUMBER OF ROUNDS OF GAME AND PICK A RANDOM PLAYER TO GO UP AGAINST THEM
+no_of_games = input('How many rounds do you wish to play for?: ')
+list_of_players = ['mordock', 'kakashi', 'inaki', 'andre', 'RPSMaster', 'josh', 'lily', 'jade', 'abigail', 'ankur']
 
-root.mainloop()
+# DISPLAY A MESSAGE SIGNIFYING SEARCH FOR A PARTNER
+print('searching for oponent...')
