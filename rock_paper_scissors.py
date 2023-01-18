e440f86da7c4b0c2efd342444a04e21accb9c2cf
Mildness Akomoize <66227187+Mildness10@users.noreply.github.com>
2023-01-17 23:55:57 -0500

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,7 +6,7 @@ def rps_game():
 
     # ALLOW PLAYER TO PICK NUMBER OF ROUNDS OF GAME AND PICK A RANDOM PLAYER TO GO UP AGAINST THEM
     no_of_games = int(input('How many rounds do you wish to play for?: '))
-    list_of_players = ['Hashirama', 'Kakashi', 'Inaki', 'Tanjiro', 'Gyomei', 'Eren', 'Lily', 'Jade', 'Abigail', 'Ankur', 'Sam', 'Joe', 'Isabella', 'Pamela', 'Wood']
+    list_of_players = ['Hashirama', 'Kakashi', 'Inaki', 'Tanjiro', 'Gyomei', 'Eren', 'Lily', 'Jade', 'Abigail', 'Ankur', 'Johan', 'Joe', 'Isabella', 'Pamela', 'Wood']
 
     # DISPLAY A MESSAGE ALONG WITH A COUNTDOWN SIGNIFYING SEARCH FOR A PARTNER
     print('Searching for opponent...')
